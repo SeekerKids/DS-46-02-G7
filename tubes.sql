05bfada8990fca0015a651bf2724c8b86406b794
SeekerKids <155258191+SeekerKids@users.noreply.github.com>
2025-01-03 00:05:37 +0700

    Add files via upload

--- tubes.sql
+++ tubes.sql
@@ -17,6 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+-- Create database and use it
+CREATE DATABASE IF NOT EXISTS `tubes`;
+USE `tubes`;
+
 --
 -- Database: `tubes`
 --
